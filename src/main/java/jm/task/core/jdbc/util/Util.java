cffe7e85c7a98a778b21fc5c879574f18ce593e5
Merichka <nik-meri@yandex.ru>
2022-03-22 19:59:57 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -19,5 +19,27 @@ public class Util {
         }
         return connection;
     }
-}
+    
+    public static SessionFactory getSessionFactory() {
+        if(sessionFactory == null) {
+            try {
+                Properties properties = new Properties();
+                properties.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                properties.setProperty("hibernate.connection.url", URL);
+                properties.setProperty("hibernate.connection.username", USER);
+                properties.setProperty("hibernate.connection.password", PASSWORD);
+                properties.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
 
+                Configuration configuration = new Configuration()
+                        .addProperties(properties).addAnnotatedClass(User.class);
+                StandardServiceRegistryBuilder builder =
+                        new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Исключение в создании sessionFactory");
+            }
+        }
+        return sessionFactory;
+    }
+}
